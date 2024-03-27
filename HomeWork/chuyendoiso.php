1e450626c93286bd8cd0e686f01ae22eb3cd85e3
Thanhcraft24 <157335652+Thanhcraft24@users.noreply.github.com>
2024-03-27 10:41:42 +0700

    Add files via upload

--- HomeWork/chuyendoiso.php
+++ HomeWork/chuyendoiso.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="file2.css">
     <title>Document</title>
+    <?php
+    $connect = mysqli_connect("localhost","root","");
+    mysqli_select_db($connect,"qlsv");
+    mysqli_set_charset($connect,"utf8");
+    $sql = "SELECT * FROM sinhvien ORDER BY masv;";
+    $kq = mysqli_query($connect,$sql);
+    ?>
 </head>
 <body>
     <div class='heading'>
@@ -14,7 +21,7 @@
             <li><a>Diễn Đàn</a></li>
             <li><a>Tin Tức</a></li>
             <li><a>Hỏi Đáp</a></li>
-            <li><a>Góc Họp Tập</a>
+            <li><a>Góc Học Tập</a>
                 <ul class='dropdown'>
                     <li><a>Chuyên đề Tin Học</a></li>
                     <li><a>Chuyên đề Âm Nhạc</a></li>
@@ -23,9 +30,72 @@
             </li>
         </ul>        
     </div>
+    
     <div class='middle'>
-        <div class='left'></div>
-        <div class='right'></div>
+        <form action="" method='POST'>
+        <div class='left'>
+            <p>Mã sinh viên: <input type='text' name='Masv'></p>
+            <p>Họ sinh viên:<input type='text' name='Ho'></p>
+            <p>Tên sinh viên:<input type='text' name='Ten'></p>
+            Nam<input type='radio' value='1' name='choose'>Nữ<input type='radio' value='0' name='choose'>
+            <input type='submit' value='nhập'>
+            <?php
+            $masv = $_POST["Masv"];
+            $hosv = $_POST["Ho"];
+            $tensv = $_POST["Ten"];
+            $phai = $_POST["choose"];
+            if ($phai == 1) {
+                $phai = 1;
+                $sql2 = "INSERT INTO sinhvien(masv, hosv, tensv, phai) VALUES ('".$masv."','".$hosv."','".$tensv."','".$phai."');";
+                mysqli_query($connect,$sql2);
+            } else {
+                $phai = 0;
+                $sql2 = "INSERT INTO sinhvien(masv, hosv, tensv, phai) VALUES ('".$masv."','".$hosv."','".$tensv."','".$phai."');";
+                mysqli_query($connect,$sql2); 
+            }
+            ?>
+        </div>
+        </form>
+        <div class='right'>
+            <?php 
+                echo "<table>
+                    <tr>
+                        <td colspan='4'>Danh sách sinh viên</td>
+                    </tr>
+                    <tr>
+                        <td>STT</td>
+                        <td>Mã sinh viên</td>
+                        <td>Tên sinh viên</td>
+                        <td>Phái</td>
+                    </tr>
+                </table>";
+                $STT=1;
+                while ($row = mysqli_fetch_row($kq)) {
+                    
+                    echo "<table>
+                        <tr>
+                            <td>".$STT."</td>
+                            <td>".$row[0]."</td>
+                            <td>".$row[1]." ".$row[2]."</td>
+                            <td>";
+                                if ($row[3]==1) {
+                                    echo "Nam";
+                                } else {
+                                    echo "Nữ";
+                                }
+                            echo "</td>
+                        </tr>
+                    </table>";
+                    $STT++;
+                }
+                echo "<table>
+                    <tr>
+                        <td colspan='4'>Tổng số sinh viên: ".mysqli_num_rows($kq)."</td>
+                    </tr>
+                </table>";
+                mysqli_close($connect);
+            ?>
+        </div>
     </div>
     <div class='footer'>
         <h2>CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BỒI DƯỠNG VĂN HOÁ</h2>
